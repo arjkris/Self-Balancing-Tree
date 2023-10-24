4247fb4b2f6c0c60d28883e13826666129ae2c4f
Arjun Krishnakumar <arjunkrishnakumar96.mec@gmail.com>
2023-10-24 15:09:25 -0500

    Fixed Height BUg and Added DeleteNode

--- AVL Tree/avlcode.java
+++ AVL Tree/avlcode.java
@@ -15,6 +15,12 @@ public class avlcode {
         root = obj.Insert(root,48);
         root = obj.Insert(root,105);
         root = obj.Insert(root,99);
+        System.out.println(root.val);
+
+        //Delete
+        root = obj.delete(root,105);
+        root = obj.delete(root,110);
+        System.out.println(root.val);
     }
 
     public class Node{
@@ -36,10 +42,10 @@ public class avlcode {
     {
         int l = 0, r = 0;
         if(root.left != null)
-            l = root.left.height;
+            l = root.left.height+1;
         if(root.right != null)
-            r = root.right.height;
-        return Math.max(l,r)+1;
+            r = root.right.height+1;
+        return Math.max(l,r);
     }
 
     public int BalanceFactor(Node root)
@@ -50,38 +56,6 @@ public class avlcode {
         if(root != null && root.right != null)
             r = root.right.height+1;
         return l-r;
-    }
-
-    public Node Insert(Node root, int val)
-    {
-        if(root == null)
-        {
-            return new Node(val);
-        }
-        if(root.val>val)
-            root.left = Insert(root.left,val);
-        else if(root.val<val)
-            root.right = Insert(root.right,val);
-
-        root.height = NodeHeight(root);
-        if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==1)
-        {
-            return LLrotation(root);
-        }
-        else if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==-1)
-        {
-            return LRrotation(root);
-        }
-        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==1)
-        {
-            return RLrotation(root);
-        }
-        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==-1)
-        {
-            return RRrotation(root);
-        }
-
-        return root;
     }
 
     public Node LLrotation(Node root)
@@ -125,5 +99,99 @@ public class avlcode {
         newroot.right.height = NodeHeight(newroot.right);
         newroot.height = NodeHeight(newroot);
         return newroot;
+    }
+
+    public Node Insert(Node root, int val)
+    {
+        if(root == null)
+        {
+            return new Node(val);
+        }
+        if(root.val>val)
+            root.left = Insert(root.left,val);
+        else if(root.val<val)
+            root.right = Insert(root.right,val);
+
+        root.height = NodeHeight(root);
+        if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==1)
+        {
+            return LLrotation(root);
+        }
+        else if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==-1)
+        {
+            return LRrotation(root);
+        }
+        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==1)
+        {
+            return RLrotation(root);
+        }
+        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==-1)
+        {
+            return RRrotation(root);
+        }
+        return root;
+    }
+
+    public Node delete(Node root, int val)
+    {
+        if(root == null)
+            return root;
+        if(root.val>val)
+            root.left = delete(root.left,val);
+        else if(root.val<val)
+            root.right = delete(root.right,val);
+        else
+        {
+            if(root.right == null || root.left == null)
+            {
+                Node temp = root.left;
+                if(temp == null)
+                    temp = root.right;
+                if(temp == null)
+                    return null;
+                root = temp;
+            }
+            else
+            {
+                Node temp = getInOrderPre(root.left);
+                root.val = temp.val;
+                root.left = delete(root.left,temp.val);
+            }
+        }
+        root.height = NodeHeight(root);
+        if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==1)
+        {
+            return LLrotation(root);
+        }
+        else if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==-1)
+        {
+            return LRrotation(root);
+        }
+        else if(BalanceFactor(root) == 2 && BalanceFactor(root.left)==0)
+        {
+            return LLrotation(root);
+        }
+        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==1)
+        {
+            return RLrotation(root);
+        }
+        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==-1)
+        {
+            return RRrotation(root);
+        }
+        else if(BalanceFactor(root) == -2 && BalanceFactor(root.right)==0)
+        {
+            return RRrotation(root);
+        }
+
+        return root;
+    }
+
+    public Node getInOrderPre(Node root)
+    {
+        Node cur = root;
+        while (cur != null && cur.right!=null)
+            cur = cur.right;
+        return cur;
     }
 }
